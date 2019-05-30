b7e653707447e097c19f7fdc20b010edf4d8cb56
Dan B <danil_iisus@icloud.com>
2019-05-30 11:42:34 -0500

    Agregar todos los sintomas  y valores al arreglo valSIntomas

--- js/chequeo.js
+++ js/chequeo.js
@@ -1,19 +1,11 @@
 var URLBASE='http://localhost/diagMed/';
 const enfermedades=[];
 enfermedades[0]=[];
-enfermedades[0]['nombre']="Hirsutismo";
-enfermedades[0]['descripcion']="primera";
-enfermedades[0]['tratamiento']="alguno";
-//////////////////////
-enfermedades[1]=[];
-enfermedades[1]['nombre']="Eccema";
-enfermedades[1]['descripcion']="primera";
-enfermedades[1]['tratamiento']="alguno";
-//////////////////////
-enfermedades[2]=[];
-enfermedades[2]['nombre']="Dermatitis atopica";
-enfermedades[2]['descripcion']="primera";
-enfermedades[2]['tratamiento']="alguno";
+enfermedades[0]['nombre']="Candidiasis";
+enfermedades[0]['descripcion']="Infección provocada por un tipo de hongo conocido como cándida y puede afectar a distintas partes del cuerpo. ";
+enfermedades[0]['tratamiento']="Fármacos antifúngicos";
+
+
 const matsintomas=[];
  matsintomas[0]=[0.15,1.6,1,0.4,0.3,0.45,0.35,0.6,0.35,0.3,0.15,0.7,0.25,0.2,0.5];
  matsintomas[1]=[0.15,1.6,1,0.4,0.3,0.45,0.35,0.6,0.2,0.3,0.15,0.7,0.25,0.2,0.5];
@@ -25,7 +17,157 @@ const matsintomas=[];
  matsintomas[7]=[0.15,1.6,1,0.3,0,0.3,0.2,0.6,0,0,0.1,0.2,0.25,0.15,0.5];
  matsintomas[8]=[0.15,1.6,1,0.15,0.3,0.15,0.1,0.6,0.15,0,0.15,0.2,0.25,0.1,0.3];
  matsintomas[9]=[0.15,0.16,0.2,0,0,0,0,0.05,0,0,0,0,0.25,0,0];
+const valSintomas=[];
 
+valSintomas[0]=[];
+valSintomas[0]['nombre']='Acne';//Sintoma 1 (0)
+valSintomas[0]['opciones']=[];
+valSintomas[0]['opciones'][0]=[];
+valSintomas[0]['opciones'][0]['text']='nada';
+valSintomas[0]['opciones'][0]['val']=0;
+valSintomas[0]['opciones'][1]=[];
+valSintomas[0]['opciones'][1]['text']='poco';
+valSintomas[0]['opciones'][1]['val']=0.5;
+
+valSintomas[1]=[];
+valSintomas[1]['nombre']="Comezon";//Sintoma 2 (1)
+valSintomas[1]['opciones']=[];
+valSintomas[1]['opciones'][0]=[];
+valSintomas[1]['opciones'][0]['text']='nada';
+valSintomas[1]['opciones'][0]['val']=0;
+valSintomas[1]['opciones'][1]=[];
+valSintomas[1]['opciones'][1]['text']='poco';
+valSintomas[1]['opciones'][1]['val']=0.5;
+
+valSintomas[2]=[];
+valSintomas[2]['nombre']="Color Amarillento";//Sintoma 3 (2)
+valSintomas[2]['opciones']=[];
+valSintomas[2]['opciones'][0]=[];
+valSintomas[2]['opciones'][0]['text']='nada';
+valSintomas[2]['opciones'][0]['val']=0;
+valSintomas[2]['opciones'][1]=[];
+valSintomas[2]['opciones'][1]['text']='poco';
+valSintomas[2]['opciones'][1]['val']=0.5;
+valSintomas[3]=[];
+
+valSintomas[3]['nombre']="Descamación";//Sintoma 4 (3)
+valSintomas[3]['opciones']=[];
+valSintomas[3]['opciones'][0]=[];
+valSintomas[3]['opciones'][0]['text']='nada';
+valSintomas[3]['opciones'][0]['val']=0;
+valSintomas[3]['opciones'][1]=[];
+valSintomas[3]['opciones'][1]['text']='poco';
+valSintomas[3]['opciones'][1]['val']=0.5;
+
+valSintomas[4]=[];
+valSintomas[4]['nombre']="Depigmentación";//Sintoma 5 (4)
+valSintomas[4]['opciones']=[];
+valSintomas[4]['opciones'][0]=[];
+valSintomas[4]['opciones'][0]['text']='nada';
+valSintomas[4]['opciones'][0]['val']=0;
+valSintomas[4]['opciones'][1]=[];
+valSintomas[4]['opciones'][1]['text']='poco';
+valSintomas[4]['opciones'][1]['val']=0.5;
+
+valSintomas[5]=[];
+valSintomas[5]['nombre']="Dolor Localizado";//Sintoma 6 (5)
+valSintomas[5]['opciones']=[];
+valSintomas[5]['opciones'][0]=[];
+valSintomas[5]['opciones'][0]['text']='nada';
+valSintomas[5]['opciones'][0]['val']=0;
+valSintomas[5]['opciones'][1]=[];
+valSintomas[5]['opciones'][1]['text']='poco';
+valSintomas[5]['opciones'][1]['val']=0.5;
+
+valSintomas[6]=[];
+valSintomas[6]['nombre']="Enrojecimiento";//Sintoma 7 (6)
+valSintomas[6]['opciones']=[];
+valSintomas[6]['opciones'][0]=[];
+valSintomas[6]['opciones'][0]['text']='nada';
+valSintomas[6]['opciones'][0]['val']=0;
+valSintomas[6]['opciones'][1]=[];
+valSintomas[6]['opciones'][1]['text']='poco';
+valSintomas[6]['opciones'][1]['val']=0.5;
+
+valSintomas[7]=[];
+valSintomas[7]['nombre']="Erupciones";//Sintoma 8 (7)
+valSintomas[7]['opciones']=[];
+valSintomas[7]['opciones'][0]=[];
+valSintomas[7]['opciones'][0]['text']='nada';
+valSintomas[7]['opciones'][0]['val']=0;
+valSintomas[7]['opciones'][1]=[];
+valSintomas[7]['opciones'][1]['text']='poco';
+valSintomas[7]['opciones'][1]['val']=0.5;
+
+valSintomas[8]=[];
+valSintomas[8]['nombre']="Lesiones Con Costra";//Sintoma 9 (8)
+valSintomas[8]['opciones']=[];
+valSintomas[8]['opciones'][0]=[];
+valSintomas[8]['opciones'][0]['text']='nada';
+valSintomas[8]['opciones'][0]['val']=0;
+valSintomas[8]['opciones'][1]=[];
+valSintomas[8]['opciones'][1]['text']='poco';
+valSintomas[8]['opciones'][1]['val']=0.5;
+
+valSintomas[9]=[];
+valSintomas[9]['nombre']="Lesiones Hipopigmentadas";//Sintoma 10 (9)
+valSintomas[9]['opciones']=[];
+valSintomas[9]['opciones'][0]=[];
+valSintomas[9]['opciones'][0]['text']='nada';
+valSintomas[9]['opciones'][0]['val']=0;
+valSintomas[9]['opciones'][1]=[];
+valSintomas[9]['opciones'][1]['text']='poco';
+valSintomas[9]['opciones'][1]['val']=0.5;
+
+valSintomas[10]=[];
+valSintomas[10]['nombre']="Mal Olor";//Sintoma 11 (10)
+valSintomas[10]['opciones']=[];
+valSintomas[10]['opciones'][0]=[];
+valSintomas[10]['opciones'][0]['text']='nada';
+valSintomas[10]['opciones'][0]['val']=0;
+valSintomas[10]['opciones'][1]=[];
+valSintomas[10]['opciones'][1]['text']='poco';
+valSintomas[10]['opciones'][1]['val']=0.5;
+
+valSintomas[11]=[];
+valSintomas[11]['nombre']="Picor";//Sintoma 12 (11)
+valSintomas[11]['opciones']=[];
+valSintomas[11]['opciones'][0]=[];
+valSintomas[11]['opciones'][0]['text']='nada';
+valSintomas[11]['opciones'][0]['val']=0;
+valSintomas[11]['opciones'][1]=[];
+valSintomas[11]['opciones'][1]['text']='poco';
+valSintomas[11]['opciones'][1]['val']=0.5;
+
+valSintomas[12]=[];
+valSintomas[12]['nombre']="Placas Cremosas";//Sintoma 13 (12)
+valSintomas[12]['opciones']=[];
+valSintomas[12]['opciones'][0]=[];
+valSintomas[12]['opciones'][0]['text']='nada';
+valSintomas[12]['opciones'][0]['val']=0;
+valSintomas[12]['opciones'][1]=[];
+valSintomas[12]['opciones'][1]['text']='poco';
+valSintomas[12]['opciones'][1]['val']=0.5;
+
+valSintomas[13]=[];
+valSintomas[13]['nombre']="Placas Rojizas";//Sintoma 14 (13)
+valSintomas[13]['opciones']=[];
+valSintomas[13]['opciones'][0]=[];
+valSintomas[13]['opciones'][0]['text']='nada';
+valSintomas[13]['opciones'][0]['val']=0;
+valSintomas[13]['opciones'][1]=[];
+valSintomas[13]['opciones'][1]['text']='poco';
+valSintomas[13]['opciones'][1]['val']=0.5;
+
+valSintomas[14]=[];
+valSintomas[14]['nombre']="Ronchas";//Sintoma 15 (14)
+valSintomas[14]['opciones']=[];
+valSintomas[14]['opciones'][0]=[];
+valSintomas[14]['opciones'][0]['text']='nada';
+valSintomas[14]['opciones'][0]['val']=0;
+valSintomas[14]['opciones'][1]=[];
+valSintomas[14]['opciones'][1]['text']='poco';
+valSintomas[14]['opciones'][1]['val']=0.5;
 
 function agregarOpcion(id){
     for (var k in enfermedades) {
