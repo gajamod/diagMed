e835f7490416471c59af49d4ea0d2431a219b263
gajamod <31940682+gajamod@users.noreply.github.com>
2019-05-30 22:17:30 -0500

    final 2

--- app/views/manual/manual.php
+++ app/views/manual/manual.php
@@ -1,2 +1,7 @@
 <h1>Manual de usuario</h1>
+<div class=""> 
+
+<a href="img/manual.pdf" target="_blank" class="m-3 mb-5">En caso de no ver el manual haga click Aqui</a>
+
+</div>
 <iframe src="img/manual.pdf" style="height:550px;" frameborder="0" class="w-100"></iframe>
